145e0be2625806a1e89e5b6ef786b512a8d30bde
mimstorefze-sketch <mimstorefze@gmail.com>
2026-06-30 11:19:06 +0400

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY.trim());
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://www.finmentra.com');
